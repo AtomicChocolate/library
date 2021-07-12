feae8cb2403e87fea039652416abad1f4c61ab59
James Stubbs <breakfastblast@gmail.com>
2021-07-12 00:50:56 -0700

    Added form validation to adding a book, updated readme, fixed bug where page reloads when adding a book, code formatting

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-A simple The Odin Project assignment to create a basic list of books. You can add and delete books, and keep track of if you've read them or not.
-I may or may not make it look better later.
+# Library
+
+[**Made as a project for The Odin Project curriculum.**](https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/library)
+A basic personal library tool that lets you add books (title, author, page count, finished or not finished) and displays all the books added in a list. Books can be deleted.
+
+## [Live Demo](https://atomicchocolate.github.io/library/)
+
+### Made with
+- HTML
+- CSS
+- JavaScript
--- script.js
+++ script.js
@@ -3,61 +3,83 @@ let form = document.querySelector("#book-form");
 let myLibrary = [];
 
 class Book {
-    constructor(title, author, pages, isRead) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.isRead = isRead;
-    }
+	constructor(title, author = "N/A", pages = 0, isRead = false) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.isRead = isRead;
+	}
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
-    let book = new Book(title, author, pages, isRead);
-    myLibrary.push(book);
+	let book = new Book(title, author, pages, isRead);
+	myLibrary.push(book);
 }
 
 function createCard(book) {
-    let card = document.createElement("div");
-    card.className = "book-card";
+	console.log("card");
+	let card = document.createElement("div");
+	card.className = "book-card";
 
-    let text = document.createTextNode(`${book.title} by ${book.author} - no. of pages: ${book.pages} - ${book.isRead ? 'Read' : 'Not read'}`);
-    card.appendChild(text);
+	let text = document.createTextNode(
+		`${book.title} by ${book.author} - no. of pages: ${book.pages} - ${
+			book.isRead ? "Read" : "Not read"
+		}`
+	);
+	card.appendChild(text);
 
-    let deleteButton = document.createElement("button");
-    deleteButton.classList.add("delete-btn");
-    deleteButton.textContent = "X";
-    card.appendChild(deleteButton);
-    
-    cards.appendChild(card);
-    return card;
+	let deleteButton = document.createElement("button");
+	deleteButton.classList.add("delete-btn");
+	deleteButton.textContent = "X";
+	card.appendChild(deleteButton);
+
+	cards.appendChild(card);
+	return card;
 }
 
 function displayLibrary() {
-   cards.innerHTML = "";
-    myLibrary.forEach(element => {
-        let card = createCard(element);
-        let button = card.getElementsByClassName("delete-btn")[0];
-        button.onclick = () => {
-            card.remove();
-            myLibrary.splice(myLibrary.indexOf(element), 1);
-        }
-    });
+	cards.innerHTML = "";
+	myLibrary.forEach((element) => {
+		let card = createCard(element);
+		let button = card.getElementsByClassName("delete-btn")[0];
+		button.onclick = () => {
+			card.remove();
+			myLibrary.splice(myLibrary.indexOf(element), 1);
+		};
+	});
+}
+
+function resetForm() {
+	document.querySelector("#form-error").textContent = "";
+	form.querySelectorAll("input").forEach((element) => {
+		element.value = "";
+	});
 }
 
 document.querySelector("#add-book-btn").onclick = () => {
-    if (form.className === "hidden") {
-        form.classList.toggle("hidden");
-    }
+	if (form.className === "hidden") {
+		form.classList.toggle("hidden");
+	}
 };
 
 document.querySelector("#submit-book-btn").onclick = () => {
-    let title = document.querySelector("#book-title").value;
-    let author = document.querySelector("#book-author").value;
-    let pages = document.querySelector("#book-pages").value;
-    let isRead = document.querySelector('input[name="is-finished"]:checked').value;
-    if (title)
-    addBookToLibrary(title, author, pages, isRead);
+	let title = document.querySelector("#book-title").value;
+	let author = document.querySelector("#book-author").value;
+	let pages = document.querySelector("#book-pages").value;
+	let isRead = document.querySelector(
+		'input[name="is-finished"]:checked'
+	).value;
+
+	if (title && author && pages && isRead) {
+		addBookToLibrary(title, author, pages, isRead);
     form.classList.toggle("hidden");
-    form.reset();
+    resetForm();
     displayLibrary();
-}
+  } else {
+		document.querySelector("#form-error").textContent =
+			"Not everything is filled out.";
+	}
+
+	// Prevents reloading the page
+	return false;
+};
